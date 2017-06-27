eaa731050311319b49c45c3fc340270186f9c7ab
Ruhan <ruhanodendaal06@gmail.com>
2017-06-27 22:29:56 +0200

    Venue&Guests

--- app/scripts/controllers/VenueCtrl.js
+++ app/scripts/controllers/VenueCtrl.js
@@ -14,4 +14,33 @@ angular.module('app.venueController', [])
       cb5: false
     };
 
+    $scope.toggleVenueGuests = function (showId,HideId) {
+      $(showId).toggle(500, "swing", function () {
+        $(HideId).toggle(500, "swing", function () {
+
+        });
+      });
+    };
+
+    // Get the modal
+    $scope.modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+    $scope.img = document.getElementById('myImg');
+    $scope.modalImg = document.getElementById("img01");
+    $scope.captionText = document.getElementById("caption");
+    $scope.img.onclick = function(){
+      $scope.modal.style.display = "block";
+      $scope.modalImg.src = this.src;
+      $scope.captionText.innerHTML = this.alt;
+    };
+
+// Get the <span> element that closes the modal
+    $scope.span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+    $scope.span.onclick = function() {
+      $scope.modal.style.display = "none";
+    }
+
   });
