8586927e6cb641136ac9d47e62a3c21f4bd67669
Ruhan Odendaal <ruhan.odendaal@investec.co.za>
2017-07-06 09:48:09 +0200

    Venue&Guests

--- app/scripts/controllers/VenueCtrl.js
+++ app/scripts/controllers/VenueCtrl.js
@@ -2,45 +2,30 @@ angular.module('app.venueController', [])
 
   .controller('VenueCtrl', function ($scope, $state) {
 
-    $scope.people = [
-      { name: 'Janet Perkins', img: 'img/100-0.jpeg', newMessage: true },
-      { name: 'Mary Johnson', img: 'img/100-1.jpeg', newMessage: false },
-      { name: 'Peter Carlsson', img: 'img/100-2.jpeg', newMessage: false }
-    ];
+    $scope.showVTop = true;
 
-    $scope.data = {
-      cb1: true,
-      cb4: true,
-      cb5: false
-    };
+    $scope.vImgClick = function (showId) {
 
-    $scope.toggleVenueGuests = function (showId,HideId) {
-      $(showId).toggle(500, "swing", function () {
-        $(HideId).toggle(500, "swing", function () {
-
+      if($scope.showVTop){
+        $(showId+'1').animate({width: 'toggle'}, 800, 'easeOutBounce', function () {
+          $(showId+'2').animate({width: 'toggle'}, 800, 'easeOutBounce', function () {
+            $(showId+'3').animate({width: 'toggle'}, 800, 'easeOutBounce', function () {
+              $scope.showVTop = false;
+            });
+          });
         });
-      });
-    };
+      } else {
+        $(showId+'3').animate({width: 'toggle'}, 500, 'easeOutBack', function () {
+          $(showId+'2').animate({width: 'toggle'}, 500, 'easeOutBack', function () {
+            $(showId+'1').animate({width: 'toggle'}, 500, 'easeOutBack', function () {
+              $scope.showVTop = true;
+            });
+          });
+        });
+      }
 
-    // Get the modal
-    $scope.modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-    $scope.img = document.getElementById('myImg');
-    $scope.modalImg = document.getElementById("img01");
-    $scope.captionText = document.getElementById("caption");
-    $scope.img.onclick = function(){
-      $scope.modal.style.display = "block";
-      $scope.modalImg.src = this.src;
-      $scope.captionText.innerHTML = this.alt;
-    };
 
-// Get the <span> element that closes the modal
-    $scope.span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-    $scope.span.onclick = function() {
-      $scope.modal.style.display = "none";
     }
 
   });
