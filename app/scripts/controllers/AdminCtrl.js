1d24eedba1eb9197f66f0feec6a7677abd4b3d16
Ruhan <ruhanodendaal06@gmail.com>
2017-08-13 21:08:36 +0200

    dashboard sessions

--- app/scripts/controllers/AdminCtrl.js
+++ app/scripts/controllers/AdminCtrl.js
@@ -1,12 +1,31 @@
 angular.module('app.adminController', [])
 
-  .controller('AdminCtrl', function ($scope,$timeout, $mdDialog) {
+  .controller('AdminCtrl', function ($scope, $timeout, $mdDialog) {
+    $scope.navItemButtons = {
+      navOverview: false,
+      navIncome: false,
+      navExpenses: false,
+      navGuests: false,
+      navOverviewMobile: false,
+      navIncomeMobile: false,
+      navExpensesMobile: false,
+      navGuestsMobile: false
+    }
+    $scope.navItemButtons.navOverview = true;
 
     $scope.clickNavItem = function (id) {
-      $scope.navItemButtons = {navOverview: false, navIncome:false, navExpenses:false, navGuests:false,navOverviewMobile: false, navIncomeMobile:false, navExpensesMobile:false, navGuestsMobile:false}
+      $scope.navItemButtons = {
+        navOverview: false,
+        navIncome: false,
+        navExpenses: false,
+        navGuests: false,
+        navOverviewMobile: false,
+        navIncomeMobile: false,
+        navExpensesMobile: false,
+        navGuestsMobile: false
+      }
       $scope.navItemButtons[id] = !$scope.navItemButtons[id]
-
-    }
+    };
 
 
     ///////////////////////////////main graphs///////////////////////////////////
@@ -73,7 +92,7 @@ angular.module('app.adminController', [])
     /////////////////Bar chart///////////////////////////
 
     $scope.labelsBar = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.seriesBar = ['Series A', 'Series B' ,'Series C'];
+    $scope.seriesBar = ['Series A', 'Series B', 'Series C'];
 
     $scope.dataBar = [
       [65, 59, 80, 81, 56, 55, 40],
@@ -116,7 +135,7 @@ angular.module('app.adminController', [])
         type: 'Office',
         number: '(555) 314-1592',
         options: {
-          face : imagePath
+          face: imagePath
         }
       },
       {
@@ -130,35 +149,35 @@ angular.module('app.adminController', [])
     ];
     $scope.todos = [
       {
-        face : imagePath,
+        face: imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
         notes: " I'll be in your neighborhood doing errands"
       },
       {
-        face : imagePath,
+        face: imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
         notes: " I'll be in your neighborhood doing errands"
       },
       {
-        face : imagePath,
+        face: imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
         notes: " I'll be in your neighborhood doing errands"
       },
       {
-        face : imagePath,
+        face: imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
         notes: " I'll be in your neighborhood doing errands"
       },
       {
-        face : imagePath,
+        face: imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
@@ -176,6 +195,109 @@ angular.module('app.adminController', [])
           $log.debug('close LEFT is done')
         })
 
+    }
+  })
+
+  .controller('IncomeCtrl', function ($scope) {
+
+    $scope.searchInput = ""
+    $scope.newItem = {type: "Monthly"};
+
+    $scope.incomeTable = [{type: "Monthly", description: 'Salary', amount: 5000},
+      {type: "Once off", description: 'Donation', amount: 3500}]
+
+    $scope.incomeOptions = ["Monthly","Once off"];
+
+    $scope.addItem = function () {
+      $scope.incomeTable.push($scope.newItem)
+    };
+
+    $scope.removeItem = function(item){
+
+    };
+
+    $scope.searchFilter = function (input) {
+      return {description:input}
+    };
+
+    $scope.totalIncome = function () {
+      var Total = 0
+      angular.forEach($scope.incomeTable,function (value) {
+        Total += value.amount
+      })
+
+      return Total
+    }
+  })
+
+  .controller('ExpenseCtrl', function ($scope) {
+
+    $scope.searchInput = ""
+    $scope.newItem = {type: "Stationary" , selected: false};
+
+    $scope.expenseTable = [{selected: true , type: "Stationary", description: 'Salary', amount: 5000 , quantity:1},
+      {selected: true, type: "Bridal attire", description: 'Donation', amount: 3500, quantity:2}]
+
+    $scope.expenseOptions = ["Stationary","Bridal attire"];
+
+    $scope.addItem = function () {
+      $scope.expenseTable.push($scope.newItem)
+    };
+
+    $scope.removeItem = function(item){
+
+    };
+
+    $scope.searchFilter = function (input) {
+      return {description:input}
+    };
+
+    $scope.totalExpense = function () {
+      var Total = 0
+
+      angular.forEach($scope.expenseTable,function (value) {
+        if(value.selected){
+          var costQuantity = value.amount * value.quantity;
+          Total += costQuantity
+        }
+      });
+
+      return Total
+    }
+  })
+
+  .controller('GuestsCtrl', function ($scope) {
+
+    $scope.searchInput = ""
+    $scope.newItem = {type: "Family" , going: false};
+
+    $scope.guestsTable = [{going: true , type: "Friend", name: 'Ruhan' ,surname:"Odendaal"},
+      {going: true , type: "Family", name: 'Boraine' ,surname:"Barnard"}]
+
+    $scope.guestsOptions = ["Family","Friend","Groom's men"];
+
+    $scope.addItem = function () {
+      $scope.guestsTable.push($scope.newItem)
+    };
+
+    $scope.removeItem = function(item){
+
+    };
+
+    $scope.searchFilter = function (input) {
+      return {name:input}
+    };
+
+    $scope.totalGuestsGoing = function () {
+      var Total = 0
+
+      angular.forEach($scope.guestsTable,function (value) {
+        if(value.going){
+          Total ++
+        }
+      });
+
+      return Total
     }
   });
 
